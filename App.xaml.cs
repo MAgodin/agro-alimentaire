381d7b1fc19a5422b72d5e69eac9063717f1c59a
Godin <134514282+MAgodin@users.noreply.github.com>
2024-10-16 11:39:40 +0200

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace agro_alimentaire
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
